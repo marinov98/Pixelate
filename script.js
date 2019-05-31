368a9c88ae9fa0eedda8af493857559fc3fcfcf6
Shanjida6 <shanjida.a@gmail.com>
2019-05-31 13:52:49 -0400

    fixed scripts

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 
-function addRow() {
-    let row = document.getElementById("myRow"); 
-    let rowCell = row.insertCell(0); 
-    rowCell.innerHTML; 
-}
-
 function addCol() {
     let col = document.getElementById("myRow"); 
     let colCell = col.insertCell(0); 
     colCell.innerHTML; 
 }
 
-function deleteRow() {
-    let delRow = document.getElementById("myRow"); 
-    let delRowCell = delRow.deleteCell(-1); /*deleteRow is func*/ 
-    delRowCell.innerHTML; 
+function addRow() {
+    let row = document.getElementById("myRow"); 
+    let rowCell = row.insertCell(0); 
+    rowCell.innerHTML; 
 }
 
 function deleteCol() {
     let delCol = document.getElementById("myRow"); 
-    let delColCell = delCol.deleteCell(-1); 
+    let delColCell = delCol.deleteCell(-1); /*deleteRow is func*/ 
     delColCell.innerHTML; 
+}
+
+function deleteRow() {
+    let delRow = document.getElementById("myRow"); 
+    let delRowCell = delRow.deleteCell(-1); 
+    delRowCell.innerHTML; 
 }
